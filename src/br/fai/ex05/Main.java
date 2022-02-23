d26771ce85db2276ce26c974f7c239a456ece053
Matuk <mateusmatuk@fai-mg.br>
2022-02-22 21:29:40 -0300

    feat: versão 1.0 do projeto

--- src/br/fai/ex05/Main.java
+++ src/br/fai/ex05/Main.java
@@ -4,14 +4,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Main app = new Main();
 		app.start();
 
 	}
-	
-	private void start(){
-		
-	}
 
+	private void start() {
+		for (int i = 0; i <= 5; i++) {
+			switch (i) {
+			case 1: {
+
+				System.out.println("Primeira condição");
+			}
+			case 4: {
+
+				System.out.println("Segunda condição");
+
+			}
+			default: {
+				System.out.println("Não encontrou uma condição");
+			}
+
+			}
+		}
+	}
 }
